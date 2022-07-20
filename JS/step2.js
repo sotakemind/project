e6e5c44b008814ee20ed97e6e8290f02fd78a4ce
Maksim <example@gmail.com>
2022-07-20 10:49:17 +0700

    Complete Step #2 and add Step3.js

---
+++ JS/step2.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
+
+
+
+
